c30a9bc77fb8db00b525825925c1b20bcf908809
subhadip-kundu <subhadipkundu106@gmail.com>
2023-12-02 23:54:34 +0530

    Compressing Some Image

---
+++ src/Components/AboutUsComponent/AboutUs/AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TopSection from '../TopSection/TopSection';
+
+function AboutUs() {
+  return (
+    <TopSection/>
+  )
+}
+
+export default AboutUs;
---
+++ src/Components/AboutUsComponent/TopSection/TopSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TopSection() {
+  return (
+        <>
+            
+        </>
+  )
+}
+
+export default TopSection;
