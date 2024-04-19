ed13dcfb1294d4cd1976e7c2a9a0819aa3cdff33
subhadip-kundu <subhadipkundu106@gmail.com>
2024-04-19 20:37:07 +0530

    Add profile functionality

--- src/Components/HomeComponent/Navbar/Navbar.jsx
+++ src/Components/HomeComponent/Navbar/Navbar.jsx
@@ -1,26 +1,30 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import DropdownIfLoggedIn from './DropdownIfLoggedIn';
-import DropdownIfLoggedOut from './DropdownIfLoggedOut';
 import companyLogo from '../../../assets/cropcompass.png';
 
 function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
   const dropdownRef = useRef(null);
+  let navigate = useNavigate();
 
   const toggleDropdown = () => {
-    setShowDropdown(!showDropdown);
+    if (isLoggedIn) {
+      setShowDropdown(!showDropdown);
+    }
   };
 
   const handleLogin = () => {
-    setIsLoggedIn(true);
     // Add your login logic here
+    if (!isLoggedIn) {
+      const reg = `register`;
+      navigate(reg);
+    }
   };
 
   const handleLogout = () => {
     setIsLoggedIn(false);
-    // Add your logout logic here
   };
 
   useEffect(() => {
@@ -39,9 +43,8 @@ function Navbar() {
     }
   };
 
-  let navigate = useNavigate(); 
-  const toHome = () =>{ 
-    let path = `/`; 
+  const toHome = () => {
+    let path = `/`;
     navigate(path);
   }
 
@@ -49,7 +52,7 @@ function Navbar() {
     <div>
       <nav className="flex justify-between w-full bg-gray-50 pl-10  pr-10 align-middle ">
         <section className="logo">
-          <img src={companyLogo} alt="Logo_image" className='w-24 cursor-pointer' onClick={toHome}/>
+          <img src={companyLogo} alt="Logo_image" className='w-24 cursor-pointer' onClick={toHome} />
         </section>
         <section className="pt-6">
           <ul>
@@ -74,15 +77,15 @@ function Navbar() {
         </section>
         <section className="flex gap-8 pt-6" ref={dropdownRef}>
           {isLoggedIn ? (
-            <React.Fragment>
+            <>
               <i className="fa-regular fa-user cursor-pointer hover:scale-105 duration-200" onClick={toggleDropdown}></i>
-              {showDropdown && <DropdownIfLoggedOut onLogout={handleLogout} />}
-            </React.Fragment>
+              {showDropdown && <DropdownIfLoggedIn onLogout={handleLogout} />}
+            </>
           ) : (
-            <React.Fragment>
+            <>
               <i className="fa-regular fa-user cursor-pointer hover:scale-105 duration-200" onClick={handleLogin}></i>
               {showDropdown && <DropdownIfLoggedIn />}
-            </React.Fragment>
+            </>
           )}
           <i className="fa-solid fa-cart-shopping cursor-pointer hover:scale-105 duration-200"></i>
         </section>
