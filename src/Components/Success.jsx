b3706d9df6557e2bd3e8095c8d65a0f0e774bc45
subhadip-kundu <subhadipkundu106@gmail.com>
2024-04-22 23:55:16 +0530

    minor change

--- src/Components/Success.jsx
+++ src/Components/Success.jsx
@@ -3,7 +3,7 @@ import submit from "../assets/Submitted.png"
 
 function Success() {
   return (
-    <div className='w-full flex items-center justify-center'>
+    <div className='w-full flex items-center justify-center h-[85vh]'>
       <img className='max-w-screen-md' src={submit} alt="Submit image" />
     </div>
   )
