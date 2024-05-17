4b3f91ffad6d1ae0cdd6fdb046d3ac0b8466b68d
subhadip-kundu <subhadipkundu106@gmail.com>
2024-05-17 20:01:02 +0530

    Working on add cart functionality

--- client/src/Components/Shop/ProductCard.jsx
+++ client/src/Components/Shop/ProductCard.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus,faBolt } from '@fortawesome/free-solid-svg-icons';
-
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus, faBolt } from "@fortawesome/free-solid-svg-icons";
 
 const ProductCard = ({ item }) => {
-  const {
-    itemName, price, block, imageUrl, quantity } = item;
+  let [addCart, setAddCart] = useState([]);
+  const { itemName, price, block, imageUrl, quantity } = item;
+  const itemSlice = { name: itemName, pri: price, area: block };
 
   const handleAddToCart = () => {
-    // Implement your add to cart logic here
-    console.log(`Added ${itemName} to cart`);
+    // console.log(`Added ${itemName} to cart`);
+    setAddCart((addCart) => [...addCart, itemSlice]);
   };
 
   const handleBuyNow = () => {
-    // Implement your buy now logic here
-    console.log(`Buying ${itemName} now`);
+    console.log(itemName);
   };
+
+  useEffect(() => {
+    addCart.map((e) => console.log(e));
+  }, [addCart]);
 
   return (
     <div className="max-w-sm  rounded overflow-hidden shadow-lg border-2 border-cyan-100 h-fit font-Rubik  bg-white">
-      <img src={imageUrl} alt={itemName} className="w-full h-52 border-b-2 border-cyan-100" />
+      <img
+        src={imageUrl}
+        alt={itemName}
+        className="w-full h-52 border-b-2 border-cyan-100"
+      />
       <div className="px-4 py-3">
         <div className="font-medium text-xl mb-2 capitalize">{itemName}</div>
         <p className="text-gray-700 text-base">Price: ₹{price}/-</p>
@@ -30,11 +37,16 @@ const ProductCard = ({ item }) => {
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-semibold text-sm py-2 px-4 rounded mr-2 flex gap-2 items-center justify-center"
           onClick={handleAddToCart}
-        ><FontAwesomeIcon icon={faCartPlus} />  Add to Cart</button>
+        >
+          <FontAwesomeIcon icon={faCartPlus} /> Add to Cart
+        </button>
         <button
           className="bg-green-500 hover:bg-green-700 text-white font-semibold text-sm py-2 px-4 rounded flex gap-2 items-center justify-center"
           onClick={handleBuyNow}
-        > <FontAwesomeIcon icon={faBolt} /> Buy Now</button>
+        >
+          {" "}
+          <FontAwesomeIcon icon={faBolt} /> Buy Now
+        </button>
       </div>
     </div>
   );
