512c7260307ae83b35da77df6f9a907c2d32c04b
subhadip-kundu <subhadipkundu106@gmail.com>
2024-05-22 00:16:51 +0530

    State recover after reloading

--- client/src/Components/HomeComponent/Home/Home.jsx
+++ client/src/Components/HomeComponent/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
 import Carousel from '../Carousel/Carousel'
 import Welcome from '../Welcome/Welcome';
 import ServicesCard from '../ServicesCard/ServicesCard';
-import Footer from '../Footer/Footer';
+import { useDispatch, useSelector } from 'react-redux';
+import { isLogged } from '../../../redux/logged/loggedSlice';
 
 function Home() {
+
+  const dispatch = useDispatch();
+
+  const isLoggedIn = useSelector((state) => state.isLogged.value);
+  if(document.cookie && !isLoggedIn){
+    dispatch(isLogged(true));
+  }
+
   return (
     <div>
       <Carousel />
