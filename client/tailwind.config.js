88d5484b5be5242f9968f3633aa1085da7e0289a
subhadip-kundu <subhadipkundu106@gmail.com>
2024-05-22 00:17:17 +0530

    add dependancy

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'about-us-topsection-bg-img': "linear-gradient(#15183f, #145b7b), url('../src/assets/Vegitable_background.jpg')"
-      }
+        "about-us-topsection-bg-img":
+          "linear-gradient(#15183f, #145b7b), url('../src/assets/Vegitable_background.jpg')",
+      },
+      animation: {
+        "spin-once": "spin-once 3s ease-in-out 1",
+      },
+      keyframes: {
+        "spin-once": {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+      fontFamily: {
+        Rubik: ['"Rubik"'],
+      },
     },
-    fontFamily: {
-      'Rubik': ['"Rubik"']
-    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
