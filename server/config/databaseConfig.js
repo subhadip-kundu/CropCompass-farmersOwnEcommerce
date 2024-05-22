6db726d99d2934aed92051c5993d86783d7b0553
subhadip-kundu <subhadipkundu106@gmail.com>
2024-05-22 17:00:22 +0530

    increase database time

--- server/config/databaseConfig.js
+++ server/config/databaseConfig.js
@@ -4,7 +4,11 @@ const MONGO_URL = process.env.MONGO_URL;
 
 const databaseConnect = () => {
   mongoose
-    .connect(MONGO_URL)
+    .connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+    })
     .then((con) => {
       console.log(con.connection.host);
     })
