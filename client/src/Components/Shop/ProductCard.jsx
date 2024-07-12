4932e68e4265bab0bb3ee3473592e6804ae85037
subhadip-kundu <subhadipkundu106@gmail.com>
2024-07-12 11:13:12 +0530

    Fix on update address

--- client/src/Components/Shop/ProductCard.jsx
+++ client/src/Components/Shop/ProductCard.jsx
@@ -14,76 +14,51 @@ const ProductCard = ({ item }) => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const calling = () => {
-    setPhoneClick(!phoneClick);
-    console.log(isAddress.data == {});
-  };
+  const calling = () => setPhoneClick(!phoneClick);
 
   const handleAddToCart = () => {
-    addToCart(item);
-    console.log(`Added ${itemName} to cart`);
+    try {
+      if (isAddress?.data?.street) {
+        addToCart(item);
+        alert('Product added successfully')
+      } else {
+        alert("Please add your address to proceed with the purchase.");
+        navigate(`/address`);
+      }
+    } catch (error) {
+      console.log("Error checking address", error);
+      alert("An error occurred while checking your address.");
+    }
   };
 
   const handleBuyNow = () => {
     setLoading(true);
-    const templateParams = {
-      order_id: id,
-      items: itemName,
-      total: price,
-    };
+    const templateParams = { order_id: id, items: itemName, total: price };
 
     emailjs
-      .send(
-        "service_39w69rp",
-        "template_q8gvj25",
-        templateParams,
-        "GyIxGuJKszHiA5GgB"
-      )
+      .send("service_39w69rp", "template_q8gvj25", templateParams, "GyIxGuJKszHiA5GgB")
       .then((response) => {
-        console.log(
-          "Order placed and email sent successfully!",
-          response.status,
-          response.text
-        );
-        setLoading(false);
         alert("Order placed and confirmation email sent successfully!");
+        setLoading(false);
       })
       .catch((error) => {
-        console.error("Failed to send email", error);
         alert("Order placed but failed to send confirmation email.");
         setLoading(false);
       });
 
-    if (!isAddress) {
-      let path = `/address`;
-      navigate(path);
+    if (!isAddress?.data?.street) {
+      navigate(`/address`);
     }
   };
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg border-2 border-cyan-100 h-fit font-Rubik bg-white">
-      <img
-        src={imageUrl}
-        alt={itemName}
-        className="w-full h-52 border-b-2 border-cyan-100"
-      />
+      <img src={imageUrl} alt={itemName} className="w-full h-52 border-b-2 border-cyan-100" />
       <div className="px-4 py-3">
         <div className="flex w-full justify-between pr-4">
-          <div className="font-medium text-xl py-2 mb-1 capitalize">
-            {itemName}
-          </div>
-          <button
-            onClick={calling}
-            className="bg-green-500 hover:bg-green-600 text-white h-fit font-semibold text-sm py-3 px-3 rounded-full flex items-center justify-center"
-          >
-            {phoneClick ? (
-              <p>
-                <FontAwesomeIcon className="rotate-45" icon={faPhone} /> &nbsp;
-                +91 {number}
-              </p>
-            ) : (
-              <FontAwesomeIcon icon={faPhone} />
-            )}
+          <div className="font-medium text-xl py-2 mb-1 capitalize">{itemName}</div>
+          <button onClick={calling} className="bg-green-500 hover:bg-green-600 text-white h-fit font-semibold text-sm py-3 px-3 rounded-full flex items-center justify-center">
+            {phoneClick ? <p><FontAwesomeIcon className="rotate-45" icon={faPhone} /> &nbsp; +91 {number}</p> : <FontAwesomeIcon icon={faPhone} />}
           </button>
         </div>
         <p className="text-gray-700 text-base">Price: ₹{price}/-</p>
@@ -91,18 +66,10 @@ const ProductCard = ({ item }) => {
         <p className="text-gray-700 text-base">Quantity: {quantity} kg</p>
       </div>
       <div className="w-full flex items-center justify-around pb-4">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-semibold text-sm py-2 px-4 rounded mr-2 flex gap-2 items-center justify-center"
-          onClick={handleAddToCart}
-        >
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold text-sm py-2 px-4 rounded mr-2 flex gap-2 items-center justify-center" onClick={handleAddToCart}>
           <FontAwesomeIcon icon={faCartPlus} /> Add to Cart
         </button>
-        <button
-          className={`${
-            loading ? "bg-orange-600" : "bg-orange-500 hover:bg-orange-600"
-          } text-white font-semibold text-sm py-2 px-4 rounded flex gap-2 items-center justify-center`}
-          onClick={handleBuyNow}
-        >
+        <button className={`${loading ? "bg-orange-600" : "bg-orange-500 hover:bg-orange-600"} text-white font-semibold text-sm py-2 px-4 rounded flex gap-2 items-center justify-center`} onClick={handleBuyNow}>
           {loading ? (
             <div className="flex items-center justify-center bg-orange-600 px-7">
               <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-white border-solid"></div>
